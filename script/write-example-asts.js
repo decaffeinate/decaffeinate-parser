18120ee0cd112f8483b5f95eb71df20706be1c66
Brian Donovan <donovan@squareup.com>
2015-10-01 19:09:31 -0700

    Allow writing the expected AST using our own parser.

--- script/write-example-asts.js
+++ script/write-example-asts.js
@@ -1,20 +1,40 @@
 #!/usr/bin/env babel-node
 
-import { join, relative } from 'path';
-import { parse as csrParse } from 'coffee-script-redux';
 import print from './print-ast';
 import { createWriteStream, readFileSync, readdirSync, statSync } from 'fs';
+import { join, relative } from 'path';
+import { parse as csrParse } from 'coffee-script-redux';
+import { parse as dcParse } from '../src/parser';
+
+let parse = source => csrParse(source, { raw: true }).toBasicObject();
+let force = false;
+
+for (let i = 2; i < process.argv.length; i++) {
+  switch (process.argv[i]) {
+    case '--force':
+    case '-f':
+      force = true;
+      break;
+
+    case '--decaffeinate':
+      parse = dcParse;
+      break;
+
+    default:
+      throw new Error(`error: unexpected argument ${process.argv[i]}`);
+  }
+}
 
 const examplesPath = join(__dirname, '../test/examples');
 readdirSync(examplesPath).forEach(entry => {
   const dir = join(examplesPath, entry);
   const astPath = join(dir, 'output.json');
-  if (!existsWithContent(astPath)) {
+  if (force || !existsWithContent(astPath)) {
     console.log(`Writing ${relative(process.cwd(), astPath)}.`);
     const input = readFileSync(join(dir, 'input.coffee'), { encoding: 'utf8' });
     const output = createWriteStream(astPath);
     const printValue = (value, stream) => stream.write(JSON.stringify(value));
-    print(csrParse(input, { raw: true }).toBasicObject(), output, printValue, printValue);
+    print(parse(input), output, printValue, printValue);
   }
 });
 
