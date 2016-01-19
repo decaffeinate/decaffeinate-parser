b331fcd007de41c7362e51d2af128a94b5ddbb38
Brian Donovan <donovan@squareup.com>
2016-01-19 11:51:05 -0800

    Write the actual JSON to a file alongside the expected.

--- test/test.js
+++ test/test.js
@@ -1,14 +1,16 @@
 import { deepEqual } from 'assert';
 import { join } from 'path';
 import { parse } from '../src/parser';
-import { readFileSync, readdirSync } from 'fs';
+import { readFileSync, readdirSync, writeFileSync } from 'fs';
 
 const examplesPath = join(__dirname, 'examples');
 readdirSync(examplesPath).forEach(entry => {
   const dir = join(examplesPath, entry);
   it(entry, () => {
-    const output = JSON.parse(readFileSync(join(dir, 'output.json'), { encoding: 'utf8' }));
     const input = readFileSync(join(dir, 'input.coffee'), { encoding: 'utf8' });
-    deepEqual(parse(input), output);
+    const actual = parse(input);
+    writeFileSync(join(dir, '_actual.json'), JSON.stringify(actual, null, 2), { encoding: 'utf8' });
+    const expected = JSON.parse(readFileSync(join(dir, 'output.json'), { encoding: 'utf8' }));
+    deepEqual(actual, expected);
   });
 });
