9143fa9746c7e07f447a040e0dee6ef6c2f94ad9
Brian Donovan <me@brian-donovan.com>
2016-03-10 13:07:41 -0800

    Use node v0.12-compatible features to allow building with that version.

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
 import babel from 'rollup-plugin-babel';
 import { readFileSync } from 'fs';
 
-const babelConfig = JSON.parse(readFileSync('.babelrc', { encoding: 'utf8' }));
-babelConfig.presets = babelConfig.presets.map(preset => preset === 'es2015' ? 'es2015-rollup' : preset);
+var babelConfig = JSON.parse(readFileSync('.babelrc', { encoding: 'utf8' }));
+babelConfig.presets = babelConfig.presets.map(function(preset) {
+  return preset === 'es2015' ? 'es2015-rollup' : preset;
+});
 babelConfig.babelrc = false;
 
 export default {
