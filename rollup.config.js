51747d4e381b8900d2f2d719107850c1f9622860
Brian Donovan <me@brian-donovan.com>
2016-06-15 11:49:07 -0700

    Use var so that we can build in node v0.12.

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 import babel from 'rollup-plugin-babel';
 import babelrc from 'babelrc-rollup';
 
-const pkg = require('./package.json');
-const external = Object.keys(pkg.dependencies);
+var pkg = require('./package.json');
+var external = Object.keys(pkg.dependencies);
 
 export default {
   entry: 'src/parser.js',
