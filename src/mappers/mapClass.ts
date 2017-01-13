b0ac251d7e05b31ad6a730e340bb228dde814652
Brian Donovan <me@brian-donovan.com>
2017-01-12 19:40:44 -0800

    refactor: add mapper for `Class` nodes
    I decided not to make any changes to the AST as part of this change, though we may want to consider it in the future. Specifically: do we really want to have another node type for the constructor? Having the constructor and bound methods already extracted for easy reference in decaffeinate is nice so it may be worth keeping those properties. Finally, `name` and `nameAssignee` are not the same thing in CoffeeScriptRedux, but they are here. We should either remove one or do the work of determining the class name and put that information in the AST. Currently decaffeinate is doing that itself. Note that we can't make `name` an identifier because of things like this: `class A::`. In that instance, the name of the class should be `prototype`, but there's no AST node that we can hook onto for that unless we make a virtual one. I added a skipped test for a case where the name isn't an identifier.

---
+++ src/mappers/mapClass.ts
@@ -0,0 +1,88 @@
+import { Assign, Class as CoffeeClass, Comment, Obj, Value } from 'decaffeinate-coffeescript/lib/coffee-script/nodes';
+import { inspect } from 'util';
+import {
+  AssignOp, Block, BoundFunction, BoundGeneratorFunction, Class, ClassProtoAssignOp, Constructor, Function,
+  Identifier, MemberAccessOp, Node, This
+} from '../nodes';
+import ParseContext from '../util/ParseContext';
+import mapAny from './mapAny';
+import mapBase from './mapBase';
+
+export default function mapClass(context: ParseContext, node: CoffeeClass): Class {
+  let { line, column, start, end, raw, virtual } = mapBase(context, node);
+
+  let body: Block | null = null;
+  let ctor: Constructor | null = null;
+  let boundMethods: Array<ClassProtoAssignOp> = [];
+
+  if (node.body && node.body.expressions.length > 0) {
+    let statements: Array<Node> = [];
+
+    for (let expression of node.body.expressions) {
+      if (expression instanceof Value && expression.base instanceof Obj) {
+        for (let property of expression.base.properties) {
+          if (property instanceof Comment) {
+            continue;
+          } else if (property instanceof Assign) {
+            let key = mapAny(context, property.variable);
+            let value = mapAny(context, property.value);
+            let Node = ClassProtoAssignOp;
+
+            if (key instanceof Identifier && key.data === 'constructor' && value instanceof Function) {
+              Node = Constructor;
+            } else if (key instanceof MemberAccessOp && key.expression instanceof This) {
+              Node = AssignOp;
+            }
+
+            let assignment = new Node(
+              key.line, key.column, key.start, value.end,
+              context.source.slice(key.start, value.end),
+              false,
+              key,
+              value
+            );
+
+            statements.push(assignment);
+
+            if (assignment instanceof ClassProtoAssignOp && (assignment.expression instanceof BoundFunction || assignment.expression instanceof BoundGeneratorFunction)) {
+              boundMethods.push(assignment);
+            }
+          } else {
+            throw new Error(`unexpected class assignment: ${inspect(property)}`);
+          }
+        }
+      } else {
+        statements.push(mapAny(context, expression));
+      }
+    }
+
+    if (statements.length > 0) {
+      let firstStatement = statements[0];
+      let lastStatement = statements[statements.length - 1];
+
+      body = new Block(
+        firstStatement.line,
+        firstStatement.column,
+        firstStatement.start,
+        lastStatement.end,
+        context.source.slice(firstStatement.start, lastStatement.end),
+        false,
+        statements,
+        false
+      );
+    }
+  }
+
+  let nameAssignee = node.variable ? mapAny(context, node.variable) : null;
+  let parent = node.parent ? mapAny(context, node.parent) : null;
+
+  return new Class(
+    line, column, start, end, raw, virtual,
+    nameAssignee,
+    nameAssignee,
+    body,
+    boundMethods,
+    parent,
+    ctor
+  );
+}
---
+++ test/examples/class-with-non-identifier-assignee/_config.js
@@ -0,0 +1,1 @@
+exports.skip = true;
