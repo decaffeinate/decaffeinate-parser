0f243ad530b52053cf0719ac9e726c05deeff93a
Alan Pierce <alangpierce@gmail.com>
2017-06-04 12:14:04 -0700

    fix: consolidate program mapping code into a mapProgram function

---
+++ src/mappers/mapProgram.ts
@@ -0,0 +1,11 @@
+import { Program } from '../nodes';
+import ParseContext from '../util/ParseContext';
+import mapPossiblyEmptyBlock from './mapPossiblyEmptyBlock';
+
+export default function mapProgram(context: ParseContext): Program {
+  return new Program(
+    1, 1, 0, context.source.length, context.source,
+    mapPossiblyEmptyBlock(context, context.ast),
+    context
+  );
+}
