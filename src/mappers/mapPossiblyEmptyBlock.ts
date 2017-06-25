b85a145e3c4cd742fc66e02ac706f424480329bb
Alan Pierce <alangpierce@gmail.com>
2017-06-25 14:44:55 -0700

    fix: create empty blocks for semicolon-only blocks
    Progress toward https://github.com/decaffeinate/decaffeinate/issues/1108
    
    Probably, the ideal behavior for blocks is to always have empty blocks rather
    than null blocks, but some of the code can't handle this yet, so this moves a
    little closer by making it so semicolon-only blocks are considered empty blocks
    rather than null. This will make it possible for decaffeinate to handle these
    nicely in a shared code path.
    
    Since these block ranges can start with whitespace, I also needed to adjust the
    start position to the first token.
    
    Also get rid of some unused code in nodes.ts.

--- src/mappers/mapPossiblyEmptyBlock.ts
+++ src/mappers/mapPossiblyEmptyBlock.ts
@@ -3,14 +3,41 @@ import { Block } from '../nodes';
 import ParseContext from '../util/ParseContext';
 import mapBlock from './mapBlock';
 
+/**
+ * Convert a CoffeeScript block node to a decaffeinate-parser Block node,
+ * carefully handling the empty block case.
+ *
+ * Ideally, there wouldn't need to be any special cases here, but there are a
+ * few things that add complexity for now.
+ * - Both decaffeinate-parser and decaffeinate expect every AST node to contain
+ *   at least one token. That means that a whitespace-only empty block will
+ *   cause a crash.
+ * - The CoffeeScript compiler seems to sometimes give incorrect location
+ *   information for empty blocks, e.g. for empty class bodies.
+ * - Blocks ending in semicolons need to sometimes have that semicolon removed
+ *   (e.g. when the block is treated as an expression), so an empty block that
+ *   is just a semicolon should become an actual empty block and not null.
+ *
+ * We convert a CoffeeScript empty block to a decaffeinate-parser empty block if
+ * the block is only a semicolon or if the block only consists of comments. All
+ * other empty blocks result in null.
+ */
 export default function mapPossiblyEmptyBlock(context: ParseContext, node: CoffeeBlock | null | undefined): Block | null {
   if (!node) {
     return null;
   }
-  // Note that, for now, we intentionally consider blocks containing only
-  // comments as non-empty, even though the statement array will have length 0.
-  // A possible future cleanup is to make those empty as well, or to generate
-  // nodes even for empty blocks.
+
+  let lastSourceIndex = context.linesAndColumns.indexForLocation({
+    line: node.locationData.last_line,
+    column: node.locationData.last_column
+  });
+  if (lastSourceIndex === null) {
+    throw new Error('Expected to find last source index of block.');
+  }
+  if (context.source[lastSourceIndex] === ';') {
+    return mapBlock(context, node);
+  }
+
   if (node.expressions.length === 0) {
     return null;
   }
